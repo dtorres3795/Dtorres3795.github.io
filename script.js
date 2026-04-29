e39eb4022dfdf1fd9e74973891bf95958b761554
dtorres3795 <dtorres3795@gmail.com>
2026-04-28 21:45:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -50,22 +50,6 @@ function generate_a_char(){
     return random_item
 }
 
-// Audio effect
-function playSound() {
-    const ctx = new AudioContext("vine-boom.mp3")
-    const osc = ctx.createOscillator()
-    const gain = ctx.createGain()
-    osc.connect(gain)
-    gain.connect(ctx.destination)
-    osc.type = "sine"
-    osc.frequency.setValueAtTime(600, ctx.currentTime)
-    osc.frequency.exponentialRampToValueAtTime(1200, ctx.currentTime + 0.15)
-    gain.gain.setValueAtTime(0.4, ctx.currentTime)
-    gain.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + 0.4)
-    osc.start(ctx.currentTime)
-    osc.stop(ctx.currentTime + 0.4)
-}
-
 function maybe(p) {
     const roll = Math.random()
     if (roll < 0.01) { playSound(); return "$ucce$$" }
